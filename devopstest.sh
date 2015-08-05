28f87f3ea3ff8cf2188f188823b64f522fdb7d27
vagrant <vagrant@ubuntu-14.04-amd64-vmwarefusion>
2015-08-05 13:52:56 +0000

    revised shell script

--- devopstest.sh
+++ devopstest.sh
@@ -3,11 +3,13 @@
 dirNum=1
 
 while [ $dirNum -lt 10 ] ; do
+
 	cd "devopsshared_0"$dirNum
 	zip "devopsshared_0"$dirNum "devopsshared_0"$dirNum"_file"[1,3,5]".txt"
 	cd ..
-	mv "devopsshared_0"$dirNum"/devopsshared_0"$dirNum.zip "devopsshared_backup/devopsshared_0"$dirNum.zip
+	mv "devopsshared_0"$dirNum"/devopsshared_0"$dirNum".zip" "devopsshared_backup/devopsshared_0"$dirNum".zip"
 	dirNum=$((dirNum + 1))
+
 done
 
 now=$(date +%F)
